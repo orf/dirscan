841e8e87ef1b0927e48bdf4dbba7006299997fa4
Tom Forbes <tom@tomforb.es>
2020-04-04 00:58:38 +0100

    Lotsa work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use ignore::{DirEntry, Error, ParallelVisitor, ParallelVisitorBuilder, WalkBuilder, WalkState};
+use indexmap::IndexSet;
 use std::collections::HashMap;
+use std::ops::Index;
 use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::time::SystemTime;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -39,107 +43,142 @@ fn main() {
 
     let threads = opt.threads.unwrap_or_else(num_cpus::get);
 
-    walker.add(PathBuf::from("lol"));
-    let mut collection_manager = CollectorManager::new();
-    walker
-        .threads(threads)
-        .build_parallel()
-        .visit(&mut collection_manager);
-}
+    let (tx, rx) = channel();
 
-struct CollectorManager {}
+    let handler = std::thread::spawn(|| {
+        let mut io_errors: u64 = 0;
+        let mut other_errors: u64 = 0;
 
-impl CollectorManager {
-    pub fn new() -> CollectorManager {
-        CollectorManager {}
-    }
-}
+        let mut path_components_set: IndexSet<String> = IndexSet::with_capacity(100);
+        let mut path_stat: HashMap<Vec<usize>, DirectoryStat> = HashMap::with_capacity(100);
 
-impl<'a> ParallelVisitorBuilder<'a> for CollectorManager {
-    fn build(&mut self) -> Box<dyn ParallelVisitor + 'a> {
-        Box::new(Collector::new())
-    }
-}
-
-struct Collector {
-    io_errors: u64,
-    other_errors: u64,
-    directory_stats: HashMap<PathBuf, DirectoryStat>,
-}
-
-impl Collector {
-    pub fn new() -> Collector {
-        let directory_stats = HashMap::with_capacity(100);
-        Collector {
-            io_errors: 0,
-            other_errors: 0,
-            directory_stats,
-        }
-    }
-}
-
-impl ParallelVisitor for Collector {
-    fn visit(&mut self, entry: Result<DirEntry, Error>) -> WalkState {
-        // unimplemented!()
-        println!("{:?} = {:?}", std::thread::current().id(), entry);
-        let dir_entry = match entry {
-            Ok(dir_entry) => dir_entry,
-            Err(Error::Io(_e)) => {
-                self.io_errors += 1;
-                return WalkState::Continue;
-            }
-            Err(_e) => {
-                self.other_errors += 1;
-                return WalkState::Continue;
-            }
-        };
-
-        // We only care about
-        if let Some(file_type) = dir_entry.file_type() {
-            if ! file_type.is_file() {
-                return WalkState::Continue;
+        for result in rx {
+            match result {
+                WalkResult::IOError => io_errors += 1,
+                WalkResult::OtherError => other_errors += 1,
+                WalkResult::File {
+                    created,
+                    accessed,
+                    modified,
+                    size,
+                    parent,
+                } => {
+                    path_components_set.insert("lol ".to_string());
+                    println!("{:?}", path_components_set.get_full(&"lol ".to_string()));
+                }
             }
         }
+    });
 
-        let entry_path = dir_entry.path();
-
-        let parent_path = match entry_path.parent() {
-            Some(parent) => parent.to_path_buf(),
-            None => {
-                self.other_errors += 1;
-                return WalkState::Continue;
+    walker.threads(threads).build_parallel().run(move || {
+        let tx_thread = tx.clone();
+        Box::new(move |entry| {
+            let dir_entry = match entry {
+                Err(Error::Io(_e)) => {
+                    tx_thread.send(WalkResult::IOError).unwrap();
+                    return WalkState::Continue;
+                }
+                Err(_e) => {
+                    tx_thread.send(WalkResult::OtherError).unwrap();
+                    return WalkState::Continue;
+                }
+                Ok(dir_entry) => dir_entry,
+            };
+            if let Some(file_type) = dir_entry.file_type() {
+                if !file_type.is_file() {
+                    return WalkState::Continue;
+                }
             }
-        };
 
-        let directory_stat = self
-            .directory_stats
-            .entry(parent_path)
-            .or_insert_with(Default::default);
+            let entry_path = dir_entry.path();
+            let parent_path = match entry_path.parent() {
+                Some(parent) => parent.to_path_buf(),
+                None => {
+                    tx_thread.send(WalkResult::OtherError).unwrap();
+                    return WalkState::Continue;
+                }
+            };
+            match dir_entry.metadata() {
+                Ok(metadata) => {
+                    tx_thread
+                        .send(WalkResult::File {
+                            created: metadata.created().ok(),
+                            accessed: metadata.accessed().ok(),
+                            modified: metadata.modified().ok(),
+                            size: metadata.len(),
+                            parent: parent_path,
+                        })
+                        .unwrap();
+                }
+                Err(e) => {
+                    tx_thread.send(WalkResult::OtherError).unwrap();
+                }
+            }
+            WalkState::Continue
+        })
+    });
 
-        directory_stat.file_count += 1;
-
-        if let Ok(metadata) = dir_entry.metadata() {
-            directory_stat.total_size += metadata.len();
-            // metadata.created()
-            // metadata.accessed()
-            // metadata.modified()
-        }
-
-        WalkState::Continue
-    }
+    handler.join();
 }
 
-impl Drop for Collector {
-    fn drop(&mut self) {
-        println!(
-"dropped"
-
-        );
-    }
+#[derive(Debug)]
+enum WalkResult {
+    IOError,
+    OtherError,
+    File {
+        created: Option<SystemTime>,
+        accessed: Option<SystemTime>,
+        modified: Option<SystemTime>,
+        size: u64,
+        parent: PathBuf,
+    },
 }
 
 #[derive(Default, Debug)]
 struct DirectoryStat {
     file_count: u64,
     total_size: u64,
+
+    latest_created: Option<SystemTime>,
+    latest_accessed: Option<SystemTime>,
+    latest_modified: Option<SystemTime>,
+}
+
+impl DirectoryStat {
+    // Please oh god tell me this can be generalized.
+    pub fn update_last_created(&mut self, created: SystemTime) {
+        match self.latest_created {
+            Some(latest_created) if latest_created < created => {
+                self.latest_created.replace(created);
+            }
+            None => {
+                self.latest_created = Some(created);
+            }
+            _ => {}
+        }
+    }
+
+    pub fn update_last_access(&mut self, accessed: SystemTime) {
+        match self.latest_accessed {
+            Some(latest_accessed) if latest_accessed < accessed => {
+                self.latest_accessed.replace(accessed);
+            }
+            None => {
+                self.latest_accessed = Some(accessed);
+            }
+            _ => {}
+        }
+    }
+
+    pub fn update_last_modified(&mut self, modified: SystemTime) {
+        match self.latest_modified {
+            Some(latest_modified) if latest_modified < modified => {
+                self.latest_modified.replace(modified);
+            }
+            None => {
+                self.latest_modified = Some(modified);
+            }
+            _ => {}
+        }
+    }
 }
